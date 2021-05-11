a43dc5d3037e39b955a9f3970e391786a82dd58d
Julian Brost <julian.brost@icinga.com>
2021-05-11 16:37:31 +0200

    Update Icinga DB MySQL schema

--- services/icingadb_mysql_schema.sql
+++ services/icingadb_mysql_schema.sql
@@ -1039,3 +1039,19 @@ CREATE TABLE history (
   INDEX idx_history_notification (notification_history_id),
   INDEX idx_history_state (state_history_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC;
+
+ALTER TABLE host_state DROP PRIMARY KEY;
+ALTER TABLE host_state ADD COLUMN id binary(20) NOT NULL FIRST;
+UPDATE host_state SET id = host_id;
+ALTER TABLE host_state ADD PRIMARY KEY (id);
+ALTER TABLE host_state ADD COLUMN properties_checksum binary(20) AFTER environment_id;
+UPDATE host_state SET properties_checksum = 0;
+ALTER TABLE host_state MODIFY COLUMN properties_checksum binary(20) NOT NULL;
+
+ALTER TABLE service_state DROP PRIMARY KEY;
+ALTER TABLE service_state ADD COLUMN id binary(20) NOT NULL FIRST;
+UPDATE service_state SET id = service_id;
+ALTER TABLE service_state ADD PRIMARY KEY (id);
+ALTER TABLE service_state ADD COLUMN properties_checksum binary(20) AFTER environment_id;
+UPDATE service_state SET properties_checksum = 0;
+ALTER TABLE service_state MODIFY COLUMN properties_checksum binary(20) NOT NULL;
