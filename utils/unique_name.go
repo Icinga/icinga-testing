c6a87c4192cae739cb8d2959d0f2ed17dff65ae3
Julian Brost <julian.brost@icinga.com>
2021-05-14 14:43:31 +0200

    Add helper to create unique object names

---
+++ utils/unique_name.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"fmt"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"testing"
+)
+
+var uniqueNameCounters sync.Map
+
+func UniqueName(t *testing.T, hint string) string {
+	prefix := t.Name()
+	if hint != "" {
+		prefix += "-" + hint
+	}
+	prefix = strings.ReplaceAll(prefix, "/", "-")
+
+	counter, _ := uniqueNameCounters.LoadOrStore(prefix, new(uint32))
+	id := atomic.AddUint32(counter.(*uint32), 1)
+
+	return fmt.Sprintf("%s-%d", prefix, id)
+}
