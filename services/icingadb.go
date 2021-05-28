478d36db7eed03d668b0eb4f9982ca066b8439d7
Julian Brost <julian.brost@icinga.com>
2021-05-28 15:17:49 +0200

    Read icingadb schema from file passed as an environment variable

--- services/icingadb.go
+++ services/icingadb.go
@@ -2,8 +2,10 @@ package services
 
 import (
 	_ "embed"
+	"fmt"
 	"github.com/go-sql-driver/mysql"
 	"io"
+	"os"
 	"regexp"
 	"strings"
 	"text/template"
@@ -41,11 +43,18 @@ func IcingaDbInstanceWriteConfig(i IcingaDbInstance, w io.Writer) error {
 	return icingadbYmlTemplate.Execute(w, i)
 }
 
-// TODO(jb): properly import schema
-//go:embed icingadb_mysql_schema.sql
-var icingadbMysqlSchema string
-
 func IcingaDbInstanceImportSchema(m MysqlDatabase) {
+	key := "ICINGA_TESTING_ICINGADB_SCHEMA"
+	schemaFile, ok := os.LookupEnv(key)
+	if !ok {
+		panic(fmt.Errorf("environment variable %s must be set", key))
+	}
+
+	schema, err := os.ReadFile(schemaFile)
+	if err != nil {
+		panic(fmt.Errorf("failed to read icingadb schema file %q: %w", schemaFile, err))
+	}
+
 	db, err := MysqlDatabaseOpen(m)
 	if err != nil {
 		panic(err)
@@ -53,7 +62,7 @@ func IcingaDbInstanceImportSchema(m MysqlDatabase) {
 	// duplicated from https://github.com/Icinga/docker-icingadb/blob/master/entrypoint/main.go
 	sqlComment := regexp.MustCompile(`(?m)^--.*`)
 	sqlStmtSep := regexp.MustCompile(`(?m);$`)
-	for _, ddl := range sqlStmtSep.Split(string(sqlComment.ReplaceAllString(icingadbMysqlSchema, "")), -1) {
+	for _, ddl := range sqlStmtSep.Split(string(sqlComment.ReplaceAll(schema, nil)), -1) {
 		if ddl = strings.TrimSpace(ddl); ddl != "" {
 			if _, err := db.Exec(ddl); err != nil {
 				if mysqlErr, ok := err.(*mysql.MySQLError); ok && mysqlErr.Number == 1050 {
